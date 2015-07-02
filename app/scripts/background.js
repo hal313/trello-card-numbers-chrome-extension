5a60cbaf8d803f2a99ba394be73db03b4585b95a
John Ghidiu <jghidiu@solutechnology.com>
2015-07-01 23:47:06 -0400

    -Modified the ability to render the page action icon

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,19 @@
+/* global chrome */
+
 (function() {
   'use strict';
 
-  // chrome.runtime.onInstalled.addListener(function (details) {});
+  // Match everything at 'trello.com' (http or https)
+  var pattern = /^http[s]?:\/\/([^\.]*\.)?trello\.com/;
 
-  chrome.tabs.onUpdated.addListener(function (tabId) {
-    // TODO: Add an icon for when card numbers will be populated
-    //chrome.pageAction.show(tabId);
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // Add an icon for when card numbers will be populated
+    //
+    // tab.url may not be available if the permission is not allowed
+    if (tab.url && tab.url.match(pattern)) {
+      chrome.pageAction.show(tabId);
+    }
+
   });
 
 }());
